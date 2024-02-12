a97c699fd651ac226fbaab6df8c501d6460157fd
Shubham Gupta <guptashubham5991@gmail.com>
2024-02-12 12:11:05 +0530

    little changes made

--- src/Components/FullName.jsx
+++ src/Components/FullName.jsx
@@ -14,7 +14,7 @@ const FullfullName = () => {
     
   return (
     <div>
-        <h1>Full fullName Display</h1>
+        <h1>Full Name Display</h1>
         <form onSubmit={handleSubmit} >
           <div>
           <label htmlFor="firstfullName">First Name:</label>
